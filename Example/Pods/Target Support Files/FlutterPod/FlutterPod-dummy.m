3244de16d65d788c60f65be1c2ecf0b61b0d9b38
Knox <nchkdxlq@gmail.com>
2020-04-19 21:59:32 +0800

    init FlutterPod

---
+++ Example/Pods/Target Support Files/FlutterPod/FlutterPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FlutterPod : NSObject
+@end
+@implementation PodsDummy_FlutterPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlutterPod_Example/Pods-FlutterPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlutterPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FlutterPod_Example
+@end
